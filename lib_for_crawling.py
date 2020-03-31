5ebbecba66f65bc83188f6abbf4b66909809352f
${Danh[0]} <49734716+shanenoi@users.noreply.github.com>
2020-03-31 20:30:01 +0700

    quởn quá nên format lại code thui

--- lib_for_crawling.py
+++ lib_for_crawling.py
@@ -3,41 +3,41 @@ import re
 import requests
 
 
-def list_url(url, function_getHtml, include, exclude):
-    '''
+def list_url(url, function_get_html, include, exclude):
+    """
         Listing URL in html code with some option
-    '''
-    
-    def fix_url(url, link):
-        if link.startswith('/'):
-            return url+link
-        else:
-            return re.findall('([^:]+://[^/]+)', url)[0] +'/'+ link
+    """
 
-    html_code = function_getHtml(url)
+    def fix_url(pattern_url, link):
+        if link.startswith('/'):
+            return pattern_url + link
+        else:
+            return re.findall('([^:]+://[^/]+)', pattern_url)[0] + '/' + link
+
+    html_code = function_get_html(url)
 
     urls = [
-        i for i in re.findall('href="([^" ]+)"', html_code) 
+        i for i in re.findall('href="([^" ]+)"', html_code)
         if (
-            True not in [j in i for j  in exclude] and
-            True in [j in i for j in include]
+                True not in [j in i for j in exclude] and
+                True in [j in i for j in include]
         )
     ]
     return [
-            link
-            if link.startswith('http') else fix_url(url, link)
-            for link in urls
+        link
+        if link.startswith('http') else fix_url(url, link)
+        for link in urls
     ]
 
 
 class CurrentTask(object):
-    '''
+    """
         [-] Save Process
-        [-] You can use it for 
+        [-] You can use it for
             [+] URL scanning
             [+] Array processing
             ...
-    '''
+    """
 
     def __init__(self, file_name):
         try:
@@ -45,43 +45,39 @@ class CurrentTask(object):
         except FileNotFoundError:
             self.file = open(file_name, 'w+')
 
-
-    def add(self, object):
+    def add(self, container):
         self.file.write(
-            self.hash_object(object)
+            self.hash_object(container)
         )
-    
-    
+
     @staticmethod
-    def __hash(value, pnum=3264):
+    def __hash(value, p_num=3264):
         ar = [str(ord(i)) for i in str(value)]
-        temp = sorted([int(''.join(ar)), pnum])
-        temp = str(temp[0]/temp[1])
-        return temp\
-                .replace('e-', '')\
-                .replace('.', '')
+        temp = sorted([int(''.join(ar)), p_num])
+        temp = str(temp[0] / temp[1])
+        return temp \
+            .replace('e-', '') \
+            .replace('.', '')
 
-
-    def hash_object(self, object):
-        return self.__hash(str(object.__repr__()
+    def hash_object(self, value):
+        return self.__hash(str(
+            value.__repr__()
         ))
 
-
-    def isdone(self, object):
+    def isdone(self, container):
         self.file.seek(0)
-        return self.hash_object(object) in self.file.read()
+        return self.hash_object(container) in self.file.read()
 
 
 class Spider(CurrentTask):
-
-    '''
-        [-] Basicly, it's just a recursion spider!
-    '''
+    """
+        [-] Basically, it's just a recursion spider!
+    """
 
     config = {
-            'list_url': list_url,
-            'include': ['.'],
-            'exclude': []
+        'list_url': list_url,
+        'include': ['.'],
+        'exclude': []
     }
     folder_content = "spider's house"
 
@@ -89,7 +85,6 @@ class Spider(CurrentTask):
         super().__init__(file_store_process)
         self.function = function_process_url
 
-    
     def get_html(self, url):
         html_code = requests.get(url).text
         '''This code will save html code to file'''
@@ -101,12 +96,11 @@ class Spider(CurrentTask):
                 './{0}/{1}'.format(
                     self.folder_content,
                     url.replace('/', '')
-                ),'w'
+                ), 'w'
         ) as f:
             f.write(re.sub('(<[^<>]+>)', '', html_code))
         '''[+]'''
         return html_code
-
 
     def bit(self, url):
         for url in self.config['list_url'](
@@ -120,11 +114,10 @@ class Spider(CurrentTask):
                 self.add(url)
                 self.bit(url)
 
-
     def find_something_in_spiderhouse(self, regex_or_word):
-        '''
-            [-] I have not optimized for multiline yet!
-        '''
+        """
+            [-] I have not optimized for multi line yet!
+        """
 
         def recursion(link):
             if os.path.isfile(link):
@@ -135,4 +128,5 @@ class Spider(CurrentTask):
             else:
                 for ld in os.listdir(link):
                     recursion(f'{link}/{ld}')
+
         recursion(self.folder_content)
